493bb6c5f7dc6393c97a034260b3c8a6b77cb9d5
op123a <107024138+op123a@users.noreply.github.com>
2022-06-07 11:30:07 +0530

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,6 +1,6 @@
 
 @echo off
 title Google Play Generator By Oafu
-echo Gen Started at %date%, %time% >> logs.txt
+echo Gen Started at %7 6 2022%, %11:28% >> logs.txt
 node App_(dont_open)
-pause
+start
